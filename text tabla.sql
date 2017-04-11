9577b9bb228cad0be95b1425e8498f693c462017
DianaMunoz <ing.dianamunoz@gmail.com>
2017-04-11 18:43:49 -0500

    CARGA INICIAL

---
+++ text tabla.sql
@@ -0,0 +1,13 @@
+.
+
+Create table TESTPRUEBA(
+--COLUMNAS
+CEDULA INT,
+APELLIDOS VARCHAR(200),
+NOMBRE VARCHAR(200),
+FECHA_N DATE,
+HORA_N TIME,
+);
+
+insert into contactos (cedula,apellidos,nombres,FECHA_N,HORA_N)
+values('Germany','Germany','Germany','Germany','Germany')
